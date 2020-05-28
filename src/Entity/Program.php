833a7a6df387d0cd3ebff2f51c54ac0661792f58
DSARRAZY <d.sarrazy@gmail.com>
2020-05-28 11:21:54 +0200

    Quete17

--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -59,6 +59,11 @@ class Program
      */
     private $actors;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $slug;
+
     public function __construct()
     {
         $this->seasons = new ArrayCollection();
@@ -177,4 +182,15 @@ class Program
         return $this;
     }
 
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
 }
